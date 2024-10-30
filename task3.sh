b395f8b48d8a179108b054c3312d3007acda8e6b
Karanpatel-1603 <kpkp16012003@gmail.com>
2024-10-30 03:34:09 +0000

    updated task3 file

--- task3.sh
+++ task3.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-gawk -F',' '($3 == "2" && $13 ~ /S/) { print $0 }' "titanic.csv" | sed 's/female/F/; s/male/M/' > "titanic_with_filled_data.csv"
-
-gawk -F',' '
-BEGIN {
-    total_age = 0;  
-    passenger_count = 0; 
-    count_temp=0
-}
-{
-    if ($7 > 0) {
-        total_age += $7; 
-        passenger_count++;   
-    }
-}
-END { average_age = (passenger_count > 0) ? total_age / passenger_count : 0;print "Average age of these passengers:", average_age;
-}' "titanic_with_filled_data.csv"
-
-
-
+gawk -F',' '{ if ($3 == 2 && $13 == "S") print $0 }' titanic.csv | \
+sed 's/female/F/; s/male/M/' | \
+gawk -F',' '{ total += $7; count += ($7 > 0) } END { print total/count }'
